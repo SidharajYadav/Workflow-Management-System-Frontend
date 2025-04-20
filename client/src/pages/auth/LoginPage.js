c3a79d0ad7fb33f6b2b75f9cdd472f1e38ef0be3
SiddharajYadav <sidharajyadav157@gmail.com>
2025-04-20 14:53:43 +0530

    Update LoginPage.js

--- client/src/pages/auth/LoginPage.js
+++ client/src/pages/auth/LoginPage.js
@@ -8,7 +8,7 @@ const LoginPage = () => {
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Log in to your Account
         </h2>
-      </div>
+      </div> 
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
