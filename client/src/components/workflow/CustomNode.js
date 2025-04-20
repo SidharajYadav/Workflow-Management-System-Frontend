be7f8d92d4a437ac1f3589dadec67c587605ca80
SiddharajYadav <sidharajyadav157@gmail.com>
2025-04-20 14:53:13 +0530

    Update CustomNode.js

--- client/src/components/workflow/CustomNode.js
+++ client/src/components/workflow/CustomNode.js
@@ -14,7 +14,7 @@ const CustomNode = ({ data }) => {
       default:
         return "bg-gray-500";
     }
-  };
+  }; 
 
   return (
     <div className="px-4 py-2 shadow-md rounded-md bg-white border-2 border-gray-200">
