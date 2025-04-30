d4a5cf3a3b62444c6efc5b3aa2129258873bb879
SiddharajYadav <sidharajyadav157@gmail.com>
2025-04-30 09:34:23 +0530

    Update LoginPage.js

--- client/src/pages/auth/LoginPage.js
+++ client/src/pages/auth/LoginPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import { SignIn } from "@clerk/clerk-react";
 
 const LoginPage = () => {
-  return (
+  return ( 
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
